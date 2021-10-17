2e4f36c8f25c6b803cdc448c22301f8330ab7206
Julian Mundhahs <julian.mundhahs@mailbox.org>
2021-10-17 15:31:59 +0200

    Change sandbox to Florence

--- ocaml_tezos_devenv/test.sh
+++ ocaml_tezos_devenv/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-eval $(opam env) && tezos-client config reset && tezos-client -A sandbox -P 20000 bootstrapped && tezos-client -A sandbox -P 20000 config update 
+sleep 15s
+eval $(opam env) && tezos-client config reset && tezos-client --endpoint http://sandbox:20000 bootstrapped && tezos-client --endpoint http://sandbox:20000 config update 
 eval $(opam env) && tezos-client import secret key alice unencrypted:edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq --force
 eval $(opam env) && tezos-client import secret key bob unencrypted:edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt --force
 #tail -F wait_for_nothing
