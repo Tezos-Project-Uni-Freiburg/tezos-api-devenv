0400068b96172ad9a675f06a64306f5314a1c454
Julian Mundhahs <julian.mundhahs@mailbox.org>
2021-10-17 15:32:52 +0200

    Autosetup ocaml repl and auto deploy demo contract

--- ocaml_tezos_devenv/test.sh
+++ ocaml_tezos_devenv/test.sh
@@ -4,5 +4,6 @@ sleep 15s
 eval $(opam env) && tezos-client config reset && tezos-client --endpoint http://sandbox:20000 bootstrapped && tezos-client --endpoint http://sandbox:20000 config update 
 eval $(opam env) && tezos-client import secret key alice unencrypted:edsk3QoqBuvdamxouPhin7swCvkQNgq4jP5KZPbwWNnwdZpSpJiEbq --force
 eval $(opam env) && tezos-client import secret key bob unencrypted:edsk3RFfvaFaxbHx8BMtEW1rKQcPtDML3LXjNqMNLCzC3wLC1bWbAt --force
+tezos-client originate contract demo transferring 0 from alice running ~/work/Documents/Studium/Uni/Bachelorprojekt/demo.tz --init '{Elt "j" 0}' --burn-cap 1
 #tail -F wait_for_nothing
 sleep infinity
